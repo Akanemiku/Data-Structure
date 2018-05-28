68e5641b68c1efd04ea7237d38ea58ce184f8738
Akanemiku <806394913@qq.com>
2018-05-28 21:37:58 +0800

    StringNet

---
+++ String/StringNet.cpp
@@ -0,0 +1,9 @@
+BF&KMP
+https://blog.csdn.net/qq_33583069/article/details/51922494
+
+KMP&Next
+https://blog.csdn.net/u011564456/article/details/20862555
+
+https://blog.csdn.net/starstar1992/article/details/54913261
+
+https://blog.csdn.net/lee18254290736/article/details/77278769
